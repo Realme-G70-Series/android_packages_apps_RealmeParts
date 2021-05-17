b6da89912666c4e28c9d27137259f5c09ff184ff
unknown <cs.officialputuid@gmail.com>
2021-05-17 12:02:03 +0800

    RealmeParts: Add title on fps meter

--- src/com/realmeparts/services/FPSInfoService.java
+++ src/com/realmeparts/services/FPSInfoService.java
@@ -226,7 +226,7 @@ public class FPSInfoService extends Service {
         }
 
         private String getFPSInfoString() {
-            return mFps;
+            return "Fps: "+mFps;
         }
 
         @Override
